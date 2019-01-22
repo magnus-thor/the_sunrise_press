91f014690f328e722eb6e41eafeff221e84c7c54
camron <camronmoradi@gmail.com>
2019-01-22 22:30:20 +0100

    Styles category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @external_articles = NewsFeedService.get_feed('all', 'cnn', 'http://us.cnn.com', 15) 
   end
 end
