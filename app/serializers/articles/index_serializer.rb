35e058e41c4106145a51cb037fb7e935751853db
Magnus Thor <magnus.thor.ar@gmail.com>
2019-02-25 13:12:07 +0100

    fixed image link in serializers

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -10,7 +10,8 @@ class Articles::IndexSerializer < ActiveModel::Serializer
         if Rails.env.test?
             rails_blob_url(object.image)
         else
-            object & image&.service_url(expires_in: 1.minute, disposition: 'inline')
+            # object & image&.service_url(expires_in: 1.minute, disposition: 'inline')
+            object.image.service_url(expires_in: 1.minute, disposition: 'inline')
         end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,40 +18,46 @@ subscriber = User.create(email: 'subscriber@mail.com', name: 'Magnus Uggla', pas
 journalist = User.create(email: 'journalist@mail.com', name: 'Christiane Amanpour', password: 'password', role: 2)
 editor = User.create(email: 'editor@mail.com', name: 'Gunde Svan', password: 'password', role: 3)
 
-2.times do 
+def attach_image(object, file_name)
+    format = File.extname(file_name).strip.downcase[1..-1]
+    file_path = Rails.root.join('app', 'assets', 'images', file_name.to_s)
+    object.image.attach(io: File.open(file_path),
+                        filename: file_name.to_s,
+                        content_type: "image/#{format}")
+end
+
+2.times do
     article = Article.create(
-        published: true, 
+        published: true,
         premium: true,
-        title: 'US shutdown: Senate rejects bills to re-open government', 
+        title: 'US shutdown: Senate rejects bills to re-open government',
         description: 'The US Senate has rejected two bills to end the government shutdown, leaving no end in sight to the record-breaking closure of federal agencies.',
         content: 'The Republican legislation failed by 50-47 and the Democratic bill followed suit by 52-44. Each bill needed 60 votes to move forward. Meanwhile, 800,000 federal workers missed another payday on Friday. At 34 days with no end in sight, this is the longest shutdown in US history.',
         journalist: journalist.name,
-        user_id: journalist.id, 
+        user_id: journalist.id,
         category_id: general.id)
-    article.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'breakingnews.png')), filename: "placeholder-image.png", content_type: 'image/png')
+    attach_image(article, "breakingnews.png")
 end
 
 2.times do
     article = Article.create(
-        published: true, 
-        title: 'Rafael Nadal crushes Stefanos Tsitsipas to reach Australian Open final', 
-        description: 'Second seed Rafael Nadal ended Greek youngster Stefanos Tsitsipas captivating run to reach his fifth Australian Open final.', 
-        content: 'Nadal, 32, was in devastating form as he beat the 20-year-old 14th seed 6-2 6-4 6-0 in one hour and 46 minutes. The Spaniard cracked 28 winners, breaking Tsitsipas serve six times and only facing one break point himself. He will play either top seed Novak Djokovic or Frances Lucas Pouille - who meet on Friday - in Sundays final.', 
+        published: true,
+        title: 'Rafael Nadal crushes Stefanos Tsitsipas to reach Australian Open final',
+        description: 'Second seed Rafael Nadal ended Greek youngster Stefanos Tsitsipas captivating run to reach his fifth Australian Open final.',
+        content: 'Nadal, 32, was in devastating form as he beat the 20-year-old 14th seed 6-2 6-4 6-0 in one hour and 46 minutes. The Spaniard cracked 28 winners, breaking Tsitsipas serve six times and only facing one break point himself. He will play either top seed Novak Djokovic or Frances Lucas Pouille - who meet on Friday - in Sundays final.',
         journalist: journalist.name,
-        user_id: journalist.id, 
+        user_id: journalist.id,
         category_id: sports.id)
-    article.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'breakingnews.png')), filename: "placeholder-image.png", content_type: 'image/png')
-
+    attach_image(article, "breakingnews.png")
 end
 
 2.times do
     article = Article.create(
-        title: Faker::GreekPhilosophers.quote, 
-        description: Faker::Lorem.paragraph_by_chars(180, false), 
-        content: Faker::Lorem.paragraph_by_chars(300, false), 
+        title: Faker::GreekPhilosophers.quote,
+        description: Faker::Lorem.paragraph_by_chars(180, false),
+        content: Faker::Lorem.paragraph_by_chars(300, false),
         journalist: journalist.name,
-        user_id: journalist.id, 
+        user_id: journalist.id,
         category_id: science.id)
-    article.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'breakingnews.png')), filename: "placeholder-image.png", content_type: 'image/png')
+    attach_image(article, "breakingnews.png")
 end
-
