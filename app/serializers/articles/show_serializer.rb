7d889f23756eb6ca476aaad404f9664bb47f000f
Magnus Thor <magnus.thor.ar@gmail.com>
2019-02-26 17:14:00 +0100

    changes image setup in show seriallizer

--- app/serializers/articles/show_serializer.rb
+++ app/serializers/articles/show_serializer.rb
@@ -9,7 +9,7 @@ class Articles::ShowSerializer < ActiveModel::Serializer
         if Rails.env.test?
             rails_blob_url(object.image)
         else
-            object & image&.service_url(expires_in: 1.minute, disposition: 'inline')
+            object.image.service_url(expires_in: 1.minute, disposition: 'inline')
         end
     end
 end
