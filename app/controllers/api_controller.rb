ce1a69d55218fc96cb9c582c4fd7fa434cc90c33
oliverochman <oliver.ochman@hotmail.com>
2019-03-20 15:40:21 +0100

    refactor API controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,4 @@
 class ApiController < ActionController::API
-    respond_to :json
     include DeviseTokenAuth::Concerns::SetUserByToken
 
-    protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }  
-    # before_action :authenticate_user!  
 end
